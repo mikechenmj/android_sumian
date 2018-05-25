e7610aa691b915a0c0430a47b79b16fbde1953ad
zhanxuzhao <xuzhao.z@sumian.com>
2018-05-25 09:05:41 +0800

    [feature][记录]：初步实现了弧形进度条；

--- app/src/main/java/com/sumian/sleepdoctor/tab/fragment/RecordFragment.java
+++ app/src/main/java/com/sumian/sleepdoctor/tab/fragment/RecordFragment.java
@@ -4,6 +4,7 @@ import android.view.View;
 
 import com.sumian.sleepdoctor.R;
 import com.sumian.sleepdoctor.base.BaseFragment;
+import com.sumian.sleepdoctor.improve.record.view.ProgressView;
 import com.sumian.sleepdoctor.improve.widget.DoctorServiceItemView;
 
 import butterknife.BindView;
@@ -11,6 +12,8 @@ import butterknife.BindView;
 public class RecordFragment extends BaseFragment {
     @BindView(R.id.dsiv)
     DoctorServiceItemView mDoctorServiceItemView;
+    @BindView(R.id.progress_view)
+    ProgressView mProgressView;
 
     @Override
     protected int getLayoutId() {
@@ -24,5 +27,7 @@ public class RecordFragment extends BaseFragment {
         mDoctorServiceItemView.setDesc("连续7天监测你的睡眠日记");
         mDoctorServiceItemView.setPrice(50f);
         mDoctorServiceItemView.loadImage(R.mipmap.ic_doctor_service_item_view_sleep_diary);
+
+        mProgressView.setPercent(95);
     }
 }
--- app/src/test/java/com/sumian/sleepdoctor/ExampleUnitTest.kt
+++ app/src/test/java/com/sumian/sleepdoctor/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.sumian.sleepdoctor
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,17 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun printDimen() {
+        for (i in 1..500) {
+            if (i % 2 == 1) {
+                continue
+            }
+            val format = String.format(Locale.getDefault(), "<dimen name=\"space_$i\">${i}dp</dimen>")
+            System.out.println(format)
+        }
+
     }
 }
