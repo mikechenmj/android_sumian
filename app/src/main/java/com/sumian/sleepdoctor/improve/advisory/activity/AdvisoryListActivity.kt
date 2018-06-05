be19f1ee74beb36b0740f5b90c7a1c9e2a078c50
yaoqi <yaoqi.y@sumian.com>
2018-06-05 18:24:19 +0800

    [feature][咨询列表]:优化咨询列表

--- app/src/main/java/com/sumian/sleepdoctor/improve/advisory/activity/AdvisoryListActivity.kt
+++ app/src/main/java/com/sumian/sleepdoctor/improve/advisory/activity/AdvisoryListActivity.kt
@@ -8,9 +8,7 @@ import android.view.View
 import com.sumian.sleepdoctor.R
 import com.sumian.sleepdoctor.base.BaseActivity
 import com.sumian.sleepdoctor.improve.advisory.bean.Advisory
-import com.sumian.sleepdoctor.improve.advisory.contract.AdvisoryContract
-import com.sumian.sleepdoctor.improve.advisory.fragment.UnusedAdvisoryFragment
-import com.sumian.sleepdoctor.improve.advisory.fragment.UsedAdvisoryFragment
+import com.sumian.sleepdoctor.improve.advisory.fragment.AdvisoryListFragment
 import com.sumian.sleepdoctor.improve.advisory.presenter.AdvisoryPresenter
 import com.sumian.sleepdoctor.widget.TitleBar
 import kotlinx.android.synthetic.main.activity_main_advisory.*
@@ -20,17 +18,13 @@ import kotlinx.android.synthetic.main.activity_main_advisory.*
  * on 2018/6/4 14:20
  * desc: 用户咨询列表
  */
-class AdvisoryListActivity : BaseActivity<AdvisoryPresenter>(), AdvisoryContract.View, TitleBar.OnBackListener {
+class AdvisoryListActivity : BaseActivity<AdvisoryPresenter>(), TitleBar.OnBackListener {
 
     companion object {
         private const val ARGS_ADVISORY_ID: String = "com.sumian.app.extras.advisory.id"
     }
 
     private var mAdvisoryId: Int = 0
-
-    override fun setPresenter(presenter: AdvisoryContract.Presenter?) {
-        this.mPresenter = presenter as AdvisoryPresenter?
-    }
 
     override fun initBundle(bundle: Bundle?): Boolean {
         this.mAdvisoryId.let {
@@ -43,11 +37,6 @@ class AdvisoryListActivity : BaseActivity<AdvisoryPresenter>(), AdvisoryContract
         return R.layout.activity_main_advisory
     }
 
-    override fun initPresenter() {
-        super.initPresenter()
-        AdvisoryPresenter.init(this)
-    }
-
     override fun initWidget(root: View?) {
         super.initWidget(root)
 
@@ -56,9 +45,11 @@ class AdvisoryListActivity : BaseActivity<AdvisoryPresenter>(), AdvisoryContract
 
             override fun getItem(position: Int): Fragment {
                 return when (position) {
-                    0 -> UnusedAdvisoryFragment()
-                    1 -> UsedAdvisoryFragment()
-                    else -> UnusedAdvisoryFragment()
+                    0 -> {
+                        AdvisoryListFragment.newInstance(Advisory.UNUSED_TYPE)!!
+                    }
+                    1 -> AdvisoryListFragment.newInstance(Advisory.USED_TYPE)!!
+                    else -> AdvisoryListFragment.newInstance(Advisory.UNUSED_TYPE)!!
                 }
             }
 
@@ -84,27 +75,13 @@ class AdvisoryListActivity : BaseActivity<AdvisoryPresenter>(), AdvisoryContract
 
     }
 
-    override fun initData() {
-        super.initData()
-        this.mPresenter.getAdvisories(Advisory.UNUSED_TYPE, mAdvisoryId)
-    }
-
     override fun onBack(v: View?) {
         finish()
     }
 
-    override fun onGetAdvisoriesSuccess(advisories: ArrayList<Advisory>) {
+    fun getAdvisoryId(): Int {
+        return this.mAdvisoryId
     }
 
-    override fun onGetAdvisoriesFailed(error: String) {
-    }
 
-    override fun onGetNextAdvisoriesSuccess(advisories: ArrayList<Advisory>) {
-    }
-
-    override fun onGetAdvisoryDetailSuccess(advisory: Advisory) {
-    }
-
-    override fun onGetAdvisoryDetailFailed(error: String) {
-    }
 }
