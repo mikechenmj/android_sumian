75913232b8a96197872abb044280228ca3170ea4
Zhan Xuzhao <xuzhao.z@sumian.com>
2018-07-19 11:04:23 +0800

    [feature][CBTI]:CBTI主页跳转周科成页；

--- app/src/main/java/com/sumian/sleepdoctor/cbti/activity/CBTIIntroductionWebActivity.kt
+++ app/src/main/java/com/sumian/sleepdoctor/cbti/activity/CBTIIntroductionWebActivity.kt
@@ -2,6 +2,7 @@ package com.sumian.sleepdoctor.cbti.activity
 
 import android.app.Activity
 import android.content.Intent
+import com.google.gson.reflect.TypeToken
 import com.sumian.sleepdoctor.base.BasePresenter
 import com.sumian.sleepdoctor.base.BaseWebViewActivity
 import com.sumian.sleepdoctor.doctor.activity.PaymentActivity
@@ -9,6 +10,7 @@ import com.sumian.sleepdoctor.doctor.bean.DoctorServiceShopData
 import com.sumian.sleepdoctor.event.CbtiServiceBoughtEvent
 import com.sumian.sleepdoctor.event.EventBusUtil
 import com.sumian.sleepdoctor.h5.H5Uri
+import com.sumian.sleepdoctor.h5.bean.H5BaseResponse
 import com.sumian.sleepdoctor.utils.JsonUtil
 import com.sumian.sleepdoctor.widget.webview.SBridgeHandler
 import com.sumian.sleepdoctor.widget.webview.SWebView
@@ -40,8 +42,20 @@ class CBTIIntroductionWebActivity : BaseWebViewActivity<BasePresenter<*>>() {
         sWebView.registerHandler("buyService", object : SBridgeHandler() {
             override fun handler(data: String?) {
                 super.handler(data)
-                val doctorService = JsonUtil.fromJson<DoctorServiceShopData>(data, DoctorServiceShopData::class.java)
-                PaymentActivity.startForResult(this@CBTIIntroductionWebActivity, doctorService, REQUEST_CODE_BUY_SERVICE)
+                val type = object : TypeToken<H5BaseResponse<DoctorServiceShopData>>() {}
+                val response = JsonUtil.fromJson<H5BaseResponse<DoctorServiceShopData>>(data, type.type)
+                        ?: return
+                PaymentActivity.startForResult(this@CBTIIntroductionWebActivity, response.result, REQUEST_CODE_BUY_SERVICE)
+            }
+        })
+        sWebView.registerHandler("onCbtiChapterClick", object : SBridgeHandler() {
+            override fun handler(data: String?) {
+                super.handler(data)
+                val type = object : TypeToken<H5BaseResponse<Map<String, Int>>>() {}
+                val response = JsonUtil.fromJson<H5BaseResponse<Map<String, Int>>>(data, type.type)
+                        ?: return
+                val chapterId = response.result?.get("chapterId") ?: return
+                CBTIWeekLessonPartActivity.show(mActivity, chapterId)
             }
         })
     }
