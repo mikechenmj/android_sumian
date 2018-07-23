cd43382aca4cc46c100df3fe05ec62a504d5b3ca
Zhan Xuzhao <xuzhao.z@sumian.com>
2018-07-23 18:47:34 +0800

    [bugfix][睡眠处方]：修复睡眠处方没谈 更新dialog的bug；

--- app/src/main/java/com/sumian/sleepdoctor/homepage/bean/SleepPrescriptionWrapper.kt
+++ app/src/main/java/com/sumian/sleepdoctor/homepage/bean/SleepPrescriptionWrapper.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class SleepPrescriptionWrapper(
         @SerializedName("sleep_prescription") val sleepPrescription: SleepPrescription?,
-        @SerializedName("update") val showUpdateDialog: Boolean,
+        @SerializedName("updated") val showUpdateDialog: Boolean,
         @SerializedName("enquire") var showEnquireDialog: Boolean,
         @SerializedName("stop_service") val isServiceStopped: Boolean,
         @SerializedName("former") val former: Boolean // 沿用之前的的
