b25c6539b25f2fe7c2df6bc80bf5176a2d76167a
Zhan Xuzhao <xuzhao.z@sumian.com>
2018-08-08 20:54:28 +0800

    [bugfix][上传日志]:token == null时不上传，避免401；

--- app/src/main/java/com/sumian/hw/log/LogJobIntentService.java
+++ app/src/main/java/com/sumian/hw/log/LogJobIntentService.java
@@ -19,6 +19,7 @@ import com.alibaba.sdk.android.oss.model.AppendObjectResult;
 import com.alibaba.sdk.android.oss.model.ObjectMetadata;
 import com.sumian.hw.network.callback.BaseResponseCallback;
 import com.sumian.sleepdoctor.BuildConfig;
+import com.sumian.sleepdoctor.account.bean.Token;
 import com.sumian.sleepdoctor.app.AppManager;
 
 import java.io.File;
@@ -60,11 +61,13 @@ public class LogJobIntentService extends JobIntentService {
             return;
         }
 
+        Token token = AppManager.getAccountViewModel().getToken();
+        if (token == null) {
+            return;
+        }
         Map<String, Object> map = new HashMap<>(0);
         map.put("suffix", "txt");
-
         Call<LogOssResponse> call = AppManager.getHwV1HttpService().autoUploadLog(map);
-
         call.enqueue(new BaseResponseCallback<LogOssResponse>() {
             @Override
             protected void onSuccess(LogOssResponse response) {
