aac3692107e4e803c4351651703e41d8d6464a15
Zhan Xuzhao <xuzhao.z@sumian.com>
2018-08-17 15:19:38 +0800

    [optimize][登录注册]:完善了输入框的逻辑；

--- app/src/main/java/com/sumian/sd/widget/CountDownTextView.kt
+++ app/src/main/java/com/sumian/sd/widget/CountDownTextView.kt
@@ -22,7 +22,7 @@ class CountDownTextView(context: Context, attrs: AttributeSet?) : TextView(conte
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.CountDownTextView)
         val normalText = attributes.getString(R.styleable.CountDownTextView_cdtv_normal_text)
         val countDownTextId = attributes.getResourceId(R.styleable.CountDownTextView_cdtv_count_down_text, 0)
-        val countDownTimeInSeconds = attributes.getInteger(R.styleable.CountDownTextView_cdtv_count_down_time, 10)
+        val countDownTimeInSeconds = attributes.getInteger(R.styleable.CountDownTextView_cdtv_count_down_time, 60)
         attributes.recycle()
         text = normalText
         mCountDownTimer = object : CountDownTimer(1000L * countDownTimeInSeconds, 1000) {
--- app/src/test/java/com/sumian/sd/KotlinTest.kt
+++ app/src/test/java/com/sumian/sd/KotlinTest.kt
@@ -2,6 +2,7 @@ package com.sumian.sd
 
 import com.google.gson.reflect.TypeToken
 import com.sumian.sd.account.bean.UserInfo
+import com.sumian.sd.account.config.SumianConfig
 import com.sumian.sd.h5.bean.H5BaseResponse
 import com.sumian.sd.utils.JsonUtil
 import org.junit.Test
@@ -18,9 +19,9 @@ import org.junit.Test
 class KotlinTest {
     @Test
     fun test() {
-        for (i in 0 until 3) {
-            System.out.println(i)
-        }
+        val regex = String.format(".{%d,%d}", SumianConfig.PASSWORD_LENGTH_MIN, SumianConfig.PASSWORD_LENGTH_MAX)
+        System.out.println(regex)
+        System.out.println("12313".matches(regex.toRegex()))
     }
 
 }
