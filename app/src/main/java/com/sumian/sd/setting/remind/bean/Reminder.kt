5e05ca817d6a7c9ba767f014cc94849973317378
uni7corn <yqicode@gmail.com>
2018-10-19 16:57:22 +0800

    [optimize][提醒设置]:为提醒设置加入注释

--- app/src/main/java/com/sumian/sd/setting/remind/bean/Reminder.kt
+++ app/src/main/java/com/sumian/sd/setting/remind/bean/Reminder.kt
@@ -8,15 +8,15 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Reminder(
         @SerializedName("id") val id: Int,
-        @SerializedName("reminder_id") val reminderId: Int, // (数据库设计的原因，所以这里用 reminder_id 表示提醒类型，1：睡眠提醒 2：睡眠日记提醒
+        @SerializedName("reminder_id") val reminderId: Int, // (数据库设计的原因，所以这里用 reminder_id 表示提醒类型，1：睡眠提醒 2：睡眠日记提醒 3:放松训练提醒
         @SerializedName("user_id") val userId: Int,
         @SerializedName("remind_at") val remindAt: String,
         @SerializedName("enable") val enable: Int
 ) : Parcelable {
 
     companion object {
-        const val SLEEP_DIARY_TYPE=2
-        const val RELAXATION_TRAINING_TYPE=3
+        const val SLEEP_DIARY_TYPE = 2
+        const val RELAXATION_TRAINING_TYPE = 3
     }
 
     fun getRemindAtHour(): Int {
