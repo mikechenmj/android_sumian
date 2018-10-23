14740024f0c2c67b9ff92bf19e1afe2c63c6b445
uni7corn <yqicode@gmail.com>
2018-10-23 11:28:10 +0800

    [bugfix][CBTI]:CBTI调查问卷结束,显示问卷结果,确定之后再开始播放视频

--- app/src/main/java/com/sumian/sd/service/cbti/dialog/CBTIQuestionDialog.kt
+++ app/src/main/java/com/sumian/sd/service/cbti/dialog/CBTIQuestionDialog.kt
@@ -1,5 +1,6 @@
 package com.sumian.sd.service.cbti.dialog
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v7.app.AppCompatDialog
 import android.view.LayoutInflater
@@ -13,10 +14,12 @@ import kotlinx.android.synthetic.main.lay_dialog_cbti_questionnaires.*
 /**
  * CBTI 调查问卷 弹窗
  */
+@SuppressLint("InflateParams")
 class CBTIQuestionDialog(context: Context) : AppCompatDialog(context, R.style.SumianDialog) {
 
     private var onSubmitQuestionCallback: OnSubmitQuestionCallback? = null
     private var mSelectPosition = -1
+    private lateinit var mCurrentQuestionnaire: Questionnaire
 
     init {
         val rootView = LayoutInflater.from(context).inflate(R.layout.lay_dialog_cbti_questionnaires, null, false)
@@ -37,10 +40,27 @@ class CBTIQuestionDialog(context: Context) : AppCompatDialog(context, R.style.Su
     }
 
     fun setQuestionnaire(questionnaire: Questionnaire): CBTIQuestionDialog {
+        this.mCurrentQuestionnaire = questionnaire
         tv_title.text = questionnaire.question
         updateItem(-1, questionnaire)
-        onSubmitQuestionCallback?.onShowQuestionDialog()
         return this
+    }
+
+    fun updateQuestionResult() {
+        val questionSelection = mCurrentQuestionnaire.selection[mSelectPosition]
+        tv_title.text = questionSelection
+        tv_submit.setText(R.string.fine)
+        tv_submit.setOnClickListener {
+            if (onSubmitQuestionCallback?.dismissQuestionDialog()!!) {
+                doCancel()
+            }
+        }
+        flow_layout.removeAllViewsInLayout()
+        val itemView = LayoutInflater.from(context).inflate(R.layout.lay_cbti_question_item, flow_layout, false)
+        val tvSelect = itemView.findViewById<TextView>(R.id.tv_select)
+        val explanation = mCurrentQuestionnaire.explanation[mSelectPosition]
+        tvSelect.text = explanation
+        flow_layout.addView(itemView)
     }
 
     private fun updateItem(position: Int, questionnaire: Questionnaire) {
@@ -68,12 +88,15 @@ class CBTIQuestionDialog(context: Context) : AppCompatDialog(context, R.style.Su
         }
     }
 
+    private fun doCancel() {
+        if (isShowing) {
+            cancel()
+        }
+    }
+
     interface OnSubmitQuestionCallback {
-
         fun submitQuestionCallback(position: Int)
-
-        fun onShowQuestionDialog()
-
+        fun dismissQuestionDialog(): Boolean
     }
 
 }
