3c48f9eafbea658c015fcc3f557487c8c0cfaeee
Zhan Xuzhao <xuzhao.z@sumian.com>
2018-10-30 13:49:22 +0800

    [bugfix][UI]: 修复了loading progress bar颜色等几个UIbug；

--- common/src/main/java/com/sumian/common/dialog/LoadingDialog.kt
+++ common/src/main/java/com/sumian/common/dialog/LoadingDialog.kt
@@ -3,6 +3,7 @@ package com.sumian.common.dialog
 import android.content.Context
 import android.support.v7.app.AppCompatDialog
 import com.sumian.common.R
+import kotlinx.android.synthetic.main.common_lay_loading_dialog.*
 
 /**
  * <pre>
@@ -15,6 +16,7 @@ import com.sumian.common.R
  */
 class LoadingDialog(ctx: Context) : AppCompatDialog(ctx, R.style.SumianLoadingDialog) {
     init {
-        setContentView(R.layout.lay_loading_dialog)
+        setContentView(R.layout.common_lay_loading_dialog)
+        common_loading_progress_bar.indeterminateDrawable.setTint(context.resources.getColor(R.color.colorPrimary))
     }
 }
