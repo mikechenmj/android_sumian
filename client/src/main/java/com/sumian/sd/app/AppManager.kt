07f0f38a56ee2f7bfbe31988624e5bc79944a7aa
uni7corn <yaoqi.y@sumian.com>
2019-02-27 11:11:15 +0800

    [client][optimize][gradle 依赖]：为了不影响协作开发效率，直接剔除 dump 依赖。该依赖只能在自己单独开分支进行优化时加入，优化完之后，需要剔除依赖再合并到协作分支

--- client/src/main/java/com/sumian/sd/app/AppManager.kt
+++ client/src/main/java/com/sumian/sd/app/AppManager.kt
@@ -12,7 +12,6 @@ import com.blankj.utilcode.util.ActivityUtils
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.blankj.utilcode.util.Utils
-import com.squareup.leakcanary.LeakCanary
 import com.sumian.blue.manager.BlueManager
 import com.sumian.common.base.BaseActivityManager
 import com.sumian.common.dns.HttpDnsEngine
@@ -178,12 +177,12 @@ object AppManager {
     }
 
     private fun initLeakCanary(app: Application) {
-        if (LeakCanary.isInAnalyzerProcess(app)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(app);
+        // if (LeakCanary.isInAnalyzerProcess(app)) {
+        // This process is dedicated to LeakCanary for heap analysis.
+        // You should not init your app in this process.
+        //     return
+        // }
+        // LeakCanary.install(app)
     }
 
     private fun initLogManager(app: Application) {
