b2d3a98df8ac51b4dab93931174fe0f2101bc10d
chenmj <minjian.c@sumian.com>
2019-09-19 13:46:29 +0800

    [patient][bug fix][review] -> 解决忧虑详情页面不会实时变化问题并规范类名称

--- patient/src/main/java/com/sumian/sd/buz/anxiousandfaith/AnxietyDetailActivity.kt
+++ patient/src/main/java/com/sumian/sd/buz/anxiousandfaith/AnxietyDetailActivity.kt
@@ -8,8 +8,11 @@ import com.sumian.sd.R
 import com.sumian.sd.buz.anxiousandfaith.bean.AnxietyData
 import com.sumian.sd.buz.anxiousandfaith.bean.AnxietyData.Companion.EXTRA_KEY_ANXIETY
 import com.sumian.sd.buz.anxiousandfaith.databinding.ActivityAnxiousDetailData
+import com.sumian.sd.buz.anxiousandfaith.event.AnxietyChangeEvent
+import com.sumian.sd.common.utils.EventBusUtil
 import com.sumian.sd.databinding.ActivityAnxietyDetailBinding
 import kotlinx.android.synthetic.main.activity_anxiety_detail.*
+import org.greenrobot.eventbus.Subscribe
 
 class AnxietyDetailActivity : WhileTitleNavBgActivity() {
 
@@ -40,12 +43,31 @@ class AnxietyDetailActivity : WhileTitleNavBgActivity() {
         super.initWidget()
         setTitle(getString(R.string.anxiety_detail_title_text))
         mTitleBar.setMenuTextDpSize(15)
-        mTitleBar.setMenuText(getString(R.string.anxiety_faith_detail_edit_menu_text))
-        mTitleBar.setOnMenuClickListener { AnxietyActivity.launch(mAnxietyData) }
+        mTitleBar.setMenuText(getString(R.string.anxiety_mood_diary_detail_edit_menu_text))
+        mTitleBar.setOnMenuClickListener { AnxietyEditActivity.launch(mAnxietyData) }
+        refreshDataBinding()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        EventBusUtil.register(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EventBusUtil.unregister(this)
+    }
+
+    @Subscribe
+    fun onAnxietyChangeEvent(event: AnxietyChangeEvent) {
+        mAnxietyData = event.anxiety
+        refreshDataBinding()
+    }
+
+    private fun refreshDataBinding() {
         var bind = DataBindingUtil.bind<ActivityAnxietyDetailBinding>(activity_anxiety_detail)
         bind?.data = ActivityAnxiousDetailData(mAnxietyData?.anxiety
                 ?: "", "问题处理类型", mAnxietyData?.solution ?: "", "2019.07.01 09:00")
-
     }
 
 }
