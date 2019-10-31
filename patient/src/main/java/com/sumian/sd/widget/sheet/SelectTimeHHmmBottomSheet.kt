ff525cf9d504b3d49a5afd1bf0da3804789469cd
chenmj <minjian.c@sumian.com>
2019-10-31 16:02:36 +0800

    [patient][bug fix] -> 忧虑时间选择器添加年份

--- patient/src/main/java/com/sumian/sd/widget/sheet/SelectTimeHHmmBottomSheet.kt
+++ patient/src/main/java/com/sumian/sd/widget/sheet/SelectTimeHHmmBottomSheet.kt
@@ -29,7 +29,7 @@ class SelectTimeHHmmBottomSheet(context: Context, @StringRes title: Int,
         private const val HOUR_SIZE = 24
         private const val MINUTE_SIZE = 60
         private const val DAY_SIZE = 365
-        private const val MMdd_PATTERN = "MM-dd"
+        private const val MMdd_PATTERN = "yyyy-MM-dd"
         val FIRST_DAY = getNextDay()
         const val DEFAULT_DAY = 0
         const val DEFAULT_HOUR = 9
