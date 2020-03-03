5ce5539eefdd073ddc7c9cf2f3652811be19a00d
chenmj <minjian.c@sumian.com>
2020-03-04 00:18:44 +0800

    [patient][重构] -> H5 分享页面修改

--- patient/src/main/java/com/sumian/sd/buz/homepage/sheet/ShareBottomSheet.kt
+++ patient/src/main/java/com/sumian/sd/buz/homepage/sheet/ShareBottomSheet.kt
@@ -3,6 +3,9 @@ package com.sumian.sd.buz.homepage.sheet
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import androidx.fragment.app.FragmentManager
+import com.sumian.common.h5.bean.WEIXIN
+import com.sumian.common.h5.bean.WEIXIN_CIRCLE
 import com.sumian.common.helper.ToastHelper
 import com.sumian.sd.R
 import com.sumian.sd.app.AppManager
@@ -27,9 +30,11 @@ class ShareBottomSheet : BaseBottomSheetView(), UMShareListener, View.OnClickLis
         private const val ARGS_MOMENT_TITLE = "com.sumian.sd.args.moment_title"
         private const val ARGS_DESC = "com.sumian.sd.args.desc"
         private const val ARGS_THUMB_URL = "com.sumian.sd.args.thumb_url"
+        private const val ARGS_WEIXIN = "com.sumian.sd.args.weixin"
+        private const val ARGS_WEIXIN_CIRCLE = "com.sumian.sd.args.weixin_circle"
 
         @JvmStatic
-        fun show(fragmentManager: androidx.fragment.app.FragmentManager, url: String, title: String, desc: String, momentTitle: String, thumbUrl: String,
+        fun show(fragmentManager: FragmentManager, url: String, title: String, desc: String, momentTitle: String, thumbUrl: String,
                  umShareListener: UMShareListener? = null) {
             val relaxationShareBottomSheet = ShareBottomSheet()
             relaxationShareBottomSheet.mListener = umShareListener
@@ -46,6 +51,22 @@ class ShareBottomSheet : BaseBottomSheetView(), UMShareListener, View.OnClickLis
                     .add(relaxationShareBottomSheet, ShareBottomSheet::class.java.simpleName)
                     .commitNowAllowingStateLoss()
         }
+
+        @JvmStatic
+        fun show(fragmentManager: FragmentManager, weixin: WEIXIN, weixinCircle: WEIXIN_CIRCLE,
+                 umShareListener: UMShareListener? = null) {
+            val relaxationShareBottomSheet = ShareBottomSheet()
+            relaxationShareBottomSheet.mListener = umShareListener
+            relaxationShareBottomSheet.arguments = Bundle().apply {
+                putParcelable(ARGS_WEIXIN, weixin)
+                putParcelable(ARGS_WEIXIN_CIRCLE, weixinCircle)
+            }
+
+            fragmentManager
+                    .beginTransaction()
+                    .add(relaxationShareBottomSheet, ShareBottomSheet::class.java.simpleName)
+                    .commitNowAllowingStateLoss()
+        }
     }
 
     private lateinit var shareUrl: String
@@ -53,6 +74,8 @@ class ShareBottomSheet : BaseBottomSheetView(), UMShareListener, View.OnClickLis
     private lateinit var shareDesc: String
     private lateinit var shareMomentTitle: String
     private lateinit var thumbUrl: String
+    private var weixin: WEIXIN? = null
+    private var weixinCircle: WEIXIN_CIRCLE? = null
     private var mListener: UMShareListener? = null
 
     override fun getLayout(): Int {
@@ -67,6 +90,8 @@ class ShareBottomSheet : BaseBottomSheetView(), UMShareListener, View.OnClickLis
             this.shareDesc = it.getString(ARGS_DESC, "")
             this.shareMomentTitle = it.getString(ARGS_MOMENT_TITLE, "")
             this.thumbUrl = it.getString(ARGS_THUMB_URL, "")
+            weixin = it.getParcelable(ARGS_WEIXIN)
+            weixinCircle = it.getParcelable(ARGS_WEIXIN_CIRCLE)
         }
     }
 
@@ -80,26 +105,52 @@ class ShareBottomSheet : BaseBottomSheetView(), UMShareListener, View.OnClickLis
     override fun onClick(v: View) {
         when (v.id) {
             R.id.tv_wechat_friend -> {
-                AppManager
-                        .getOpenEngine()
-                        .shareUrl(activity!!,
-                                shareUrl,
-                                shareTitle,
-                                shareDesc,
-                                thumbUrl,
-                                SHARE_MEDIA.WEIXIN,
-                                this@ShareBottomSheet)
+                if (weixin != null) {
+                    var data = weixin!!
+                    AppManager
+                            .getOpenEngine()
+                            .shareUrl(activity!!,
+                                    data.link,
+                                    data.title,
+                                    data.desc,
+                                    data.imgUrl,
+                                    SHARE_MEDIA.WEIXIN,
+                                    this@ShareBottomSheet)
+                } else {
+                    AppManager
+                            .getOpenEngine()
+                            .shareUrl(activity!!,
+                                    shareUrl,
+                                    shareTitle,
+                                    shareDesc,
+                                    thumbUrl,
+                                    SHARE_MEDIA.WEIXIN,
+                                    this@ShareBottomSheet)
+                }
             }
             R.id.tv_wechat_circle -> {
-                AppManager
-                        .getOpenEngine()
-                        .shareUrl(activity!!,
-                                shareUrl,
-                                shareMomentTitle,
-                                shareDesc,
-                                thumbUrl,
-                                SHARE_MEDIA.WEIXIN_CIRCLE,
-                                this@ShareBottomSheet)
+                if (weixinCircle != null) {
+                    var data = weixinCircle!!
+                    AppManager
+                            .getOpenEngine()
+                            .shareUrl(activity!!,
+                                    data.link,
+                                    data.title,
+                                    "",
+                                    data.imgUrl,
+                                    SHARE_MEDIA.WEIXIN_CIRCLE,
+                                    this@ShareBottomSheet)
+                } else {
+                    AppManager
+                            .getOpenEngine()
+                            .shareUrl(activity!!,
+                                    shareUrl,
+                                    shareMomentTitle,
+                                    shareDesc,
+                                    thumbUrl,
+                                    SHARE_MEDIA.WEIXIN_CIRCLE,
+                                    this@ShareBottomSheet)
+                }
             }
             R.id.tv_cancel -> {
             }
--- patient/src/main/java/com/sumian/sd/common/h5/ChannelH5Fragment.kt
+++ patient/src/main/java/com/sumian/sd/common/h5/ChannelH5Fragment.kt
@@ -1,9 +1,17 @@
 package com.sumian.sd.common.h5
 
+import android.view.View
 import com.sumian.common.h5.BaseWebViewFragment
+import com.sumian.common.h5.bean.H5BindShareData
+import com.sumian.common.h5.widget.SWebView
 import com.sumian.common.h5.widget.SWebViewLayout
+import com.sumian.common.statistic.StatUtil
 import com.sumian.sd.BuildConfig
 import com.sumian.sd.R
+import com.sumian.sd.buz.homepage.sheet.ShareBottomSheet
+import com.sumian.sd.buz.stat.StatConstants
+import com.umeng.socialize.UMShareListener
+import com.umeng.socialize.bean.SHARE_MEDIA
 import kotlinx.android.synthetic.main.channel_h5_fragment.*
 
 class ChannelH5Fragment : BaseWebViewFragment() {
@@ -20,5 +28,33 @@ class ChannelH5Fragment : BaseWebViewFragment() {
         val token = "?token=" + getToken()
         val completeUrl = BuildConfig.CHANNEL_H5_URL + token
         return completeUrl
+    }
+
+    override fun registerHandler(sWebView: SWebView) {
+        sWebView.registerHandler("bindShare") { data, function ->
+            val shareData = H5BindShareData.fromJson(data)
+            getSWebViewLayout().shareView.visibility = View.VISIBLE
+            getSWebViewLayout().shareView.setOnClickListener {
+                if (shareData.weixin == null || shareData.weixinCircle == null) {
+                    return@setOnClickListener
+                }
+                ShareBottomSheet.show(childFragmentManager, shareData.weixin!!, shareData.weixinCircle!!,
+                        object : UMShareListener {
+                            override fun onResult(p0: SHARE_MEDIA?) {
+
+                            }
+
+                            override fun onCancel(p0: SHARE_MEDIA?) {
+                            }
+
+                            override fun onError(p0: SHARE_MEDIA?, p1: Throwable?) {
+                            }
+
+                            override fun onStart(p0: SHARE_MEDIA?) {
+                                StatUtil.event(StatConstants.on_relaxation_detail_page_share_success)
+                            }
+                        })
+            }
+        }
     }
 }
